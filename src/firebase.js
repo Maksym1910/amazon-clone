962dfce69ba12ff91d0acfd4a146ec32d49f80f1
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2020-12-11 21:05:35 +0200

    initial project setup / firebase connected

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDPmeej9MccyeGSCUai-RLceb63gB8dqCY',
+  authDomain: 'clone-d488b.firebaseapp.com',
+  projectId: 'clone-d488b',
+  storageBucket: 'clone-d488b.appspot.com',
+  messagingSenderId: '627082621716',
+  appId: '1:627082621716:web:5f4dc7600db8aeb4e48801',
+  measurementId: 'G-L4R3VB33X2',
+};
